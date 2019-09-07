2ec124e820b829c3643a9f2a4056ffd624dc66a7
jwang3vsu <jwang3vsu@gmail.com>
2019-09-07 00:10:02 -0400

    widows.py target ball added

--- widows.py
+++ widows.py
@@ -20,13 +20,14 @@ def adddomino(p):
                       baseVisualShapeIndex=visualShapeId,
                       basePosition=[-.5,  -2, 0.14],
                       useMaximalCoordinates=True)
-  p.resetBaseVelocity(objid,linearVelocity=[2,2,1])
+  p.resetBaseVelocity(objid,linearVelocity=[0,10,1])
 
 p.loadURDF("table_s/table.urdf", -.5000000, -2.00000, -.820000, 0.000000, 0.000000, 0.0, 1.0)
 #p.setGravity(0, 0, -10)
 arm = p.loadURDF("widowx/gun.urdf", useFixedBase=1)
 #arm = p.loadURDF("widowx/widowx.urdf", basePosition=[-.5, -2, 0.01], baseOrientation=[0,0,0,1])
-#arm2 = p.loadURDF("sphere2.urdf", basePosition=[-.5, -2, 1], baseOrientation=[0,0,0,1])
+ball = p.loadURDF("sphere2.urdf", useFixedBase=1)
+p.resetBasePositionAndOrientation(ball, [-.5, 2, 0.1], [0,0,0,1])
 p.setGravity(0, 0, -10)
 
 p.resetBasePositionAndOrientation(arm, [-0.098612, -2, 0.14018],
@@ -41,8 +42,8 @@ while (1):
   time.sleep(0.01)
   #p.saveWorld("test.py")
   viewMat = p.getDebugVisualizerCamera()[2]
-  #projMatrix = [0.7499999403953552, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0]
-  projMatrix = p.getDebugVisualizerCamera()[3]
+  projMatrix = [0.7499999403953552, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 2.0, 2.0, -0.02000020071864128, 2.0]
+  #projMatrix = p.getDebugVisualizerCamera()[3]
   width = 640
   height = 480
   img_arr = p.getCameraImage(width=width,
